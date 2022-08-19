1748c8f8465405c01c38f4c86d05aa8b49a8d87f
Rachmannh <rachmannul1999@gmail.com>
2022-08-19 16:41:38 +0700

    commit

---
+++ landing-page/src/views/ContactView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="contact">
+    <div class="container">
+      <div class="card">
+        <img src="../assets/logo.png" class="card-img-top" alt="Hero Image" />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
